3373c2509eae26d431bb87e8ed1d7f0e9e735893
Abdullah2000Habib <abd3habib@gmail.com>
2023-01-11 12:26:00 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./main.css";
+
+
+
+
+function App() {
+  return (
+    <div className='App'>
+        <div>hi</div>
+    </div>
+  );
+  
+  }
+
+export default App;
